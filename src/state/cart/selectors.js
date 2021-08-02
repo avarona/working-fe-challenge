3ac375740a49e65b6763148269aaec2782b61dce
Alex <alejandro@cubesoftware.com>
2021-08-02 15:45:45 -0400

    remove cart & catalog selectors

--- src/state/cart/selectors.js
+++ src/state/cart/selectors.js
@@ -1,5 +1,1 @@
-import { createSelector } from "@reduxjs/toolkit";
-
 export const selectCart = (state) => state.cart;
-
-export const selectItems = createSelector(selectCart, (cart) => cart.items);
